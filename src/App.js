42f7e9de475580689ec56009e67bd9f6323b27d8
Matthew Eldredge <mattheweldredge@gmail.com>
2017-12-01 12:13:31 -0700

    step 4

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      friends: [],
+      picture: '',
+      name: '',
+    }
+  }
+
   render() {
     return (
       <div>
